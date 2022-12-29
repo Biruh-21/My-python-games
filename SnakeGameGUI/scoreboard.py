6ada128e3b4485ddd5be0fc51749af794fbc0568
Biruh Tesfaye <57385970+Biruh-21@users.noreply.github.com>
2022-12-29 10:12:06 -0300

    enabling the game to hold a high score

--- SnakeGameGUI/scoreboard.py
+++ SnakeGameGUI/scoreboard.py
@@ -6,17 +6,28 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.highscore = int(file.read())
         self.penup()
         self.hideturtle()
         self.goto(0, 280)
         self.color("white")
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
     def add_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
-    def game_over(self):
-        self. goto(0,0)
-        self.write("GAME OVER!", False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode='w') as file:
+                file.write(str(self.highscore))
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}, Highscore: {self.highscore}", False, align=ALIGNMENT, font=FONT)
+ 
